be07e5c47461995bcf928eda64e34d71f481efb5
Andrea Mannucci <a.mannucci@reply.it>
2022-12-01 15:11:10 +0100

    removed pods

--- Pods/Target Support Files/Pods-AzureUI SampleUITests/Pods-AzureUI SampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AzureUI_SampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_AzureUI_SampleUITests
-@end
--- Pods/Target Support Files/Pods-AzureUIWrapper/Pods-AzureUIWrapper-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AzureUIWrapper : NSObject
-@end
-@implementation PodsDummy_Pods_AzureUIWrapper
-@end
